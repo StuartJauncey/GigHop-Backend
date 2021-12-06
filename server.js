a5100027bac5f7bd4e1dc1b3ef48808e5f03961f
Dominic <dominicfrancisharris@gmail.com>
2021-12-06 12:39:45 +0000

    Sets up connection to server

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config({ path: "./config.env" });
+
+const express = require("express");
+const cors = require("cors");
+
+// get MongoDB connection
+const dbo = require("./connection");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// App Routers
+app.use();
+app.use("/api", apiRouter);
+
+// Error Handling
+app.use(function (err, req, res) {
+	console.error(err.stack);
+	res.status(500).send("Server error");
+});
+
+// Connect to database when server starts
+dbo.connectToServer(function (err) {
+	if (err) {
+		console.error(err);
+		process.exit();
+	}
+
+	// Start Express server
+	app.listen(PORT, () => {
+		console.log(`Server running on port: ${PORT}`);
+	});
+});
