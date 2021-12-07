b7db227f392fdb67fa558e2c7eaa4ba53940e3aa
Dominic <dominicfrancisharris@gmail.com>
2021-12-07 11:33:13 +0000

    adds venues requests

--- controllers/venues-controllers.js
+++ controllers/venues-controllers.js
@@ -1,8 +1,8 @@
 // Helps us connect to database
+const { ObjectId } = require("bson");
 const dbo = require("../connection");
 
 exports.getAllVenues = (req, res) => {
-	console.log(process.env.ATLAS_URI);
 	const dbConnect = dbo.getDb();
 
 	dbConnect
@@ -14,4 +14,59 @@ exports.getAllVenues = (req, res) => {
 		.toArray((err, result) => {
 			res.json(result);
 		});
+};
+
+exports.postNewVenue = async (req, res) => {
+	const dbConnect = dbo.getDb();
+	const newObj = req.body;
+	const expectedKeys = [
+		"venue_name",
+		"coordinates",
+		"description",
+		"picture",
+		"address",
+		"upcoming_events",
+	];
+
+	for (let i = 0; i < expectedKeys.length; i++) {
+		if (!Object.keys(newObj).includes(expectedKeys[i])) {
+			return res.status(400).send({ status: 400, message: "Invalid Data Key" });
+		}
+	}
+
+	await dbConnect
+		.collection("Venues")
+		.insertOne(newObj, function (err, result) {
+			if (err) {
+				res.status(400).send("Error inserting matches!");
+			} else {
+				console.log(`Added a new match with id ${result.insertedId}`);
+				res.status(204).send(newObj);
+			}
+		});
+};
+
+exports.patchVenueWithEvent = async (req, res) => {
+	const dbConnect = dbo.getDb();
+
+	const updateObject = req.body;
+	const id = req.params.venue_id;
+
+	console.log(id);
+
+	await dbConnect.collection("Venues").updateOne(
+		{ _id: ObjectId(id) },
+		// UPDATE OBJECT IN REQUEST: { "upcoming_events": "Another event" }
+		{ $push: updateObject },
+		function (err, _result) {
+			if (err) {
+				res
+					.status(400)
+					.send(`Error updating events on venue with id ${listingQuery.id}!`);
+			} else {
+				console.log("Document updated");
+				res.status(200).send(req.body);
+			}
+		}
+	);
 };
--- routes/venues-router.js
+++ routes/venues-router.js
@@ -1,7 +1,13 @@
-const { getAllVenues } = require("../controllers/venues-controllers");
+const {
+	getAllVenues,
+	postNewVenue,
+	patchVenueWithEvent,
+} = require("../controllers/venues-controllers");
 
 const venuesRouter = require("express").Router();
 
 venuesRouter.route("/").get(getAllVenues);
+venuesRouter.route("/").post(postNewVenue);
+venuesRouter.route("/:venue_id").patch(patchVenueWithEvent);
 
 module.exports = venuesRouter;
