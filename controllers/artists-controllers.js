dc9582195b3bcfa353e08f655bd2765ca4d9c135
Stuart Jauncey <stuart.jauncey@shift.online>
2021-12-08 10:12:48 +0000

    adds get single artist and post new artist endpoints

--- controllers/artists-controllers.js
+++ controllers/artists-controllers.js
@@ -1,7 +1,9 @@
+const { ObjectId } = require("bson");
 const dbo = require("../connection");
 
 exports.getAllArtists = (req, res) => {
   const dbConnect = dbo.getDb();
+
   dbConnect
     .collection("Artists")
     .find({})
@@ -11,5 +13,42 @@ exports.getAllArtists = (req, res) => {
     })
     .catch((err) => {
       console.log(err);
+    })
+}
+
+exports.getArtist = (req, res) => {
+  const dbConnect = dbo.getDb();
+  const id = req.params.artist_id;
+
+  dbConnect
+    .collection("Artists")
+    .findOne({ _id: ObjectId(id)})
+    .then((result) => {
+      return res.status(200).send(result);
+    })
+  // needs error handing for invalid ids
+}
+
+exports.postNewArtist = async (req, res) => {
+  const dbConnect = dbo.getDb();
+  const newArtist = req.body;
+  const expectedKeys = [
+    "artist_name",
+    "description",
+    "picture"
+  ]
+
+  expectedKeys.forEach((key) => {
+    if (!Object.keys(newArtist).includes(key)) {
+      return res.status(400).send("Invalid Artist Syntax")
+    }
+  })
+
+  await dbConnect
+    .collection("Artists")
+    .insertOne(newArtist)
+    .then((result) => {
+      console.log("Added new artist")
+      res.status(204).send(result);
     })
 }
--- routes/artists-router.js
+++ routes/artists-router.js
@@ -1,7 +1,9 @@
-const { getAllArtists } = require("../controllers/artists-controllers");
+const { getAllArtists, getArtist, postNewArtist } = require("../controllers/artists-controllers");
 
 const artistsRouter = require("express").Router();
 
 artistsRouter.route("/").get(getAllArtists);
+artistsRouter.route("/:artist_id").get(getArtist);
+artistsRouter.route("/").post(postNewArtist);
 
 module.exports = artistsRouter;
