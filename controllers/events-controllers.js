e1007183370611b63438bdd62b04efd41a538810
Stuart Jauncey <stuart.jauncey@shift.online>
2021-12-13 11:02:05 +0000

    readds date modifier

--- controllers/events-controllers.js
+++ controllers/events-controllers.js
@@ -92,6 +92,13 @@ exports.getEvent = (req, res) => {
 exports.postNewEvent = async (req, res) => {
   const dbConnect = dbo.getDb();
   const newObj = req.body;
+
+  const startTime = new Date(newObj.time_start);
+  const endTime = new Date(newObj.time_end);
+
+  newObj.time_start = startTime;
+  newObj.time_end = endTime;
+
   const expectedKeys = [
     "event_name",
     "entry_price",
